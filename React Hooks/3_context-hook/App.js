626c739aff640e8791770e7da2baa869176cd1f1
Himanshu Mishra <mhimanshu712@gmail.com>
2021-03-31 20:19:54 +0530

    ReducerHook documented

--- React Hooks/3_context-hook/App.js
+++ React Hooks/3_context-hook/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import './App.css'
-import ComponentC from './components/ComponentC'
+import ComponentC from './ComponentC'
 
 export const UserContext = React.createContext()
 export const ChannelContext = React.createContext()
